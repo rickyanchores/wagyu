3a069afb0c36e21a2b6efeb328792f4619471ae3
RickyAnchores <ricky23anchores@gmail.com>
2024-05-20 10:31:12 +0200

    edit

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='Banner bg-slate-600 font-bold p-4'>
-        <div className="container text-white text-center">
+    <div className='Banner bg-zinc-900 font-bold p-4'>
+        <div className="container text-zinc-500 text-2xl text-center flex justify-around">
             <h1>Uber Eats</h1>
+            <h1>Glovo</h1>
+            <h1>Deliveroo</h1>
         </div>
     </div>
   )
